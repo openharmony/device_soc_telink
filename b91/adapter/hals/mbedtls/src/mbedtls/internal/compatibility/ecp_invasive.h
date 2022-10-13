78ef6f8628ac5f5b64f46b6da013508833b3aba2
Dmytro Kosmii <dmytro.kosmii@telink-semi.com>
2022-10-13 10:00:46 +0300

    License header fix.
    Signed-off-by: Dmytro Kosmii <dmytro.kosmii@telink-semi.com>
    Change-Id: Ia395ab77b615af5d87211253d635264df82e4b5d

--- b91/adapter/hals/mbedtls/src/mbedtls/internal/compatibility/ecp_invasive.h
+++ b91/adapter/hals/mbedtls/src/mbedtls/internal/compatibility/ecp_invasive.h
@@ -1,28 +1,21 @@
-/**
- * \file ecp_invasive.h
+/******************************************************************************
+ * Copyright (c) 2022 Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ * All rights reserved.
  *
- * \brief ECP module: interfaces for invasive testing only.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The interfaces in this file are intended for testing purposes only.
- * They SHOULD NOT be made available in library integrations except when
- * building the library for testing.
- */
-/*
- *  Copyright The Mbed TLS Contributors
- *  SPDX-License-Identifier: Apache-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may
- *  not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+ *****************************************************************************/
+
 #ifndef MBEDTLS_ECP_INVASIVE_H
 #define MBEDTLS_ECP_INVASIVE_H
 
