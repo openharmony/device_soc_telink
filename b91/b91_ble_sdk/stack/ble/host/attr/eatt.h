7188bc5c8c97a29ede59db054bd79bac09cb1419
dmytrokashkarov <dmytro.kashkarov@telink-semi.com>
2023-05-05 16:54:04 +0300

    UTCSH-234: update SDK to multiconnection
    Signed-off-by: dmytrokashkarov <dmytro.kashkarov@telink-semi.com>
    Change-Id: I53afc904d8de89a2f0e8ef2caba3a370588b237e

--- b91/b91_ble_sdk/stack/ble/host/attr/eatt.h
+++ b91/b91_ble_sdk/stack/ble/host/attr/eatt.h
@@ -1,24 +1,20 @@
-/********************************************************************************************************
- * @file     eatt.h
+/******************************************************************************
+ * Copyright (c) 2022 Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ * All rights reserved.
  *
- * @brief    for TLSR chips
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @author   BLE Group
- * @date     Sep. 18, 2019
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * @par      Copyright (c) Telink Semiconductor (Shanghai) Co., Ltd.
- *           All rights reserved.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *           The information contained herein is confidential and proprietary property of Telink
- *           Semiconductor (Shanghai) Co., Ltd. and is available under the terms
- *           of Commercial License Agreement between Telink Semiconductor (Shanghai)
- *           Co., Ltd. and the licensee in separate contract or the terms described here-in.
- *           This heading MUST NOT be removed from this file.
- *
- *           Licensees are granted free, non-transferable use of the information in this
- *           file under Mutual Non-Disclosure Agreement. NO WARRENTY of ANY KIND is provided.
- *
- *******************************************************************************************************/
+ *****************************************************************************/
 #ifndef GATT_H_
 #define GATT_H_
 
